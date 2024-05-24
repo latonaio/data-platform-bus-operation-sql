eeee644af69d64e20131893e8af80e9d74d25e46
misupopo <kokorononakaniitumo@yahoo.co.jp>
2024-05-24 11:20:20 +0900

    no message

--- data-platform-bus-operation-sql-item-data.sql
+++ data-platform-bus-operation-sql-item-data.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `data_platform_bus_operation_item_data`
 (
   `BusLine`                      int(16) NOT NULL,
-  `BusOperationVersion`          int(4) NOT NULL,
-  `WeekdayType`                  varchar(6) NOT NULL,
+  `OperationDate`                date NOT NULL,
   `BusOperationID`               int(4) NOT NULL,
-  `ExpressType`                  varchar(4) NOT NULL,
   `BusLineBusStopID`             int(4) NOT NULL,
   `BusStop`                      int(16) NOT NULL,
-  `DepartingTime`                time(16) NOT NULL,
-  `ArrivingTime`                 time(16) DEFAULT NULL,
+  `PlannedArrivalTime`           time DEFAULT NULL,
+  `PlannedDepartureTime`         time NOT NULL,
+  `ActualArrivalTime`            time DEFAULT NULL,
+  `ActualDepartureTime`          time NOT NULL,
   `Description`                  varchar(60) NOT NULL,
   `OperationRemarks`             varchar(100) DEFAULT NULL,
   `CreationDate`                 date NOT NULL,
@@ -20,9 +20,9 @@ CREATE TABLE `data_platform_bus_operation_item_data`
   `IsReleased`                   tinyint(1) DEFAULT NULL,
   `IsMarkedForDeletion`          tinyint(1) DEFAULT NULL,
 
-    PRIMARY KEY (`BusLine`, `BusOperationVersion`, `WeekdayType`, `BusOperationID`, `ExpressType`, `BusLineBusStopID`, `BusStop`),
+    PRIMARY KEY (`BusLine`, `OperationDate`, `BusOperationID`, `BusLineBusStopID`),
 
-    CONSTRAINT `DPFMBusOperationItemData_fk` FOREIGN KEY (`BusLine`, `BusOperationVersion`, `WeekdayType`, `BusOperationID`, `ExpressType`) REFERENCES `data_platform_bus_operation_header_data` (`BusLine`, `BusOperationVersion`, `WeekdayType`, `BusOperationID`, `ExpressType`),
+    CONSTRAINT `DPFMBusOperationItemData_fk` FOREIGN KEY (`BusLine`, `OperationDate`, `BusOperationID`) REFERENCES `data_platform_bus_operation_header_data` (`BusLine`, `OperationDate`, `BusOperationID`),
     CONSTRAINT `DPFMBusOperationItemDataBusStop_fk` FOREIGN KEY (`BusLine`, `BusLineBusStopID`, `BusStop`) REFERENCES `data_platform_bus_line_bus_stop_data` (`BusLine`, `BusLineBusStopID`, `BusStop`),
     CONSTRAINT `DPFMBusOperationItemDataCreateUser_fk` FOREIGN KEY (`CreateUser`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
     CONSTRAINT `DPFMBusOperationItemDataLastChangeUser_fk` FOREIGN KEY (`LastChangeUser`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`)
